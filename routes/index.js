b62a062597bec330c8914580936f2ee5e4c461b7
Dimitri Leurs <dimitri.leurs@numberly.com>
2020-11-24 11:18:17 +0100

    Database connected

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.QOVERY_DATABASE_MY_DB_CONNECTION_URI,
+})
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
